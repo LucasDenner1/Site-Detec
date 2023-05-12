3ae10312ed4b3758c6d0a9aa6034dddccab063d4
Thiago Frank <luffyzao1000@gmail.com>
2023-05-12 12:16:46 -0300

    Alinhei as DIV ao centro

--- Home.php
+++ Home.php
@@ -56,39 +56,39 @@ if (empty($_SESSION['intruso'])) {
     }
   </script>
 
-  <div id="addPubli" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo"> <!-- Div de adicionar algo  // iago -->
-    <img id="mais" src="./imagens/maism.png">
-  </div>
-
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Publicação</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form id="modalForm" method="post" action="publicacao.php">
-            <div class="form-group">
-              <label for="recipient-name" class="col-form-label">TITULO:</label>
-              <input  name="titulo" type="text" class="form-control" id="recipient-name">
-            </div>
-            <div class="form-group">
-              <label for="message-text" class="col-form-label">TEXTO:</label>
-              <textarea name="texto" class="form-control" id="message-text"></textarea>
-            </div>
-            <input type="hidden" name="data_hora" value="<?php echo date('Y-m-d H:i:s'); ?>">
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-primary">ENVIAR</button>
-            </div>
-          </form>
+    <div id="addPubli" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo"> 
+      <img id="mais" src="./imagens/maism.png">
+    </div>
+  
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Publicação</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
+          <div class="modal-body">
+            <form id="modalForm" method="post" action="publicacao.php">
+              <div class="form-group">
+                <label for="recipient-name" class="col-form-label">TITULO:</label>
+                <input  name="titulo" type="text" class="form-control" id="recipient-name">
+              </div>
+              <div class="form-group">
+                <label for="message-text" class="col-form-label">TEXTO:</label>
+                <textarea name="texto" class="form-control" id="message-text"></textarea>
+              </div>
+              <input type="hidden" name="data_hora" value="<?php echo date('Y-m-d H:i:s'); ?>">
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">ENVIAR</button>
+              </div>
+            </form>
+            </div>
+        </div>
       </div>
     </div>
-  </div>
 
   <?php
    $sql = "SELECT * FROM publi ORDER BY hora DESC";
