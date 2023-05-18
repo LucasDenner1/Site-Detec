cd63ba61cd241d226c2f4027c3d6eb1719a0972f
Thiago Frank <luffyzao1000@gmail.com>
2023-05-18 14:54:28 -0300

    Fim da estilização da caixa de publicação

--- Home.php
+++ Home.php
@@ -97,7 +97,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content" class="corpoModal">
+      <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">PUBLICAÇÃO</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
